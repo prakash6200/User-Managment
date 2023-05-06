43265e109b02eb59ec8740ceb4266e3e489ba681
Prakash kumar <prakashkumar97068@gmail.com>
2023-05-06 22:34:58 +0530

    Validated recharge data

--- recharge/router/auth.route.js
+++ recharge/router/auth.route.js
@@ -4,9 +4,10 @@ const router = express.Router();
 const { verifyJWTToken } = require("../../middleware/jwt.middleware");
 const stateList = require("../../utils/utils.controller");
 const rechargeController = require("../controller/recharge.controller");
+const rechargeValidation = require("../validator/recharge.validator");
 
 router.get("/state/list", verifyJWTToken, stateList.stateList);
 router.get("/company/list", verifyJWTToken, stateList.companyList);
-router.post("/recharge", verifyJWTToken, rechargeController.mroboticsRechage);
+router.post("/recharge", rechargeValidation.mroboticsRechage, verifyJWTToken, rechargeController.mroboticsRechage);
 
 module.exports = router;
--- recharge/validator/recharge.validator.js
+++ recharge/validator/recharge.validator.js
@@ -1,53 +1,18 @@
 const Joi = require("joi");
 
-// module.exports.register = async (request, response, next) => {
-//     let rules = Joi.object().keys({
-//             email: Joi.string().required(),
-//             mobile: Joi.string().length(10).pattern(/^[0-9]+$/).required(),
-//             password: Joi.string().required(),
-//             name: Joi.string().required(),
-//             role: Joi.string().required(),
-//     });
-//     const { error } = rules.validate(request.body);
-//     if (error) {
-//         return response
-//             .status(422)
-//             .json({ status: false, message: error, data: null });
-//     } else {
-//         return next();
-//     }
-// };
-
-module.exports.login = async (request, response, next) => {
-    const { mobile } = request.body;
-
-    if(!mobile){
-        let rules = Joi.object().keys({
-            email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
-            password: Joi.string().required(),
-        });
-    
-        const { error } = rules.validate(request.body);
-        if (error) {
-            return response
-                .status(422)
-                .json({ status: false, message: error.message, data: null });
-        } else {
-            return next();
-        }
+module.exports.mroboticsRechage = async (request, response, next) => {
+    let rules = Joi.object().keys({
+        mobile: Joi.string().length(10).pattern(/^[0-9]+$/).required(),
+        amount: Joi.number().required(),
+        companyId: Joi.number().integer().min(1).max(27).required(),
+        is_stv: Joi.boolean().required(),
+    });
+    const { error } = rules.validate(request.body);
+    if (error) {
+        return response
+            .status(422)
+            .json({ status: false, message: error, data: null });
     } else {
-        let rules = Joi.object().keys({
-            mobile: Joi.string().length(10).pattern(/^[0-9]+$/).required(),
-            password: Joi.string().required(),
-        });
-    
-        const { error } = rules.validate(request.body);
-        if (error) {
-            return response
-                .status(422)
-                .json({ status: false, message: error.message, data: null });
-        } else {
-            return next();
-        }
+        return next();
     }
 };
