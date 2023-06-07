ede2b1947e2f7cf2ed5b5afa521ab9c916ca7973
Prakash kumar <prakashkumar97068@gmail.com>
2023-06-08 00:09:02 +0530

    AEPS balance enquery

--- retailer/router/aeps.router.js
+++ retailer/router/aeps.router.js
@@ -8,5 +8,6 @@ router.post("/biometric/ekyc", verifyJWTToken, aepsController.aepsBiometricEKYC)
 router.get("/bank/list", verifyJWTToken, aepsController.aepsBanks);
 router.post("/ministatement", verifyJWTToken, aepsController.aepsMiniStatement);
 router.post("/cash/withdrawal", verifyJWTToken, aepsController.aepsCashWithdrawal);
+router.post("/balance/enquery", verifyJWTToken, aepsController.aepsBalanceEnquery);
 
 module.exports = router;
